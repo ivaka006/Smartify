693ef25a19b51436517b4fee3d0e8a096ff322cc
Ivaylo <ivcho.belovski@gmail.com>
2025-11-13 01:59:59 +0200

    almost working register

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -28,6 +28,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
